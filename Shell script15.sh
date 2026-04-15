aa0b850585ed9b8b6ff4dec690cfa1ff4b28388f
Adityakhiratkar30 <khiratkaraditya785@gmail.com>
2026-04-15 09:44:23 +0530

    Create Shell script15.sh

---
+++ Shell script15.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo " Enter the option "
+echo "a = Print the date "
+echo "b = list all files in current directory "
+read choice
+case $choice in 
+a) date;;
+b) ls;;
+*) echo " Not a valid option "
+esac 
+
+
